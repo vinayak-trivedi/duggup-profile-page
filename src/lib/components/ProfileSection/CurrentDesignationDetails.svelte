692ef3a60c37d5faf9977bfef5704505b7792bb2
vinayak <trivedivinayak57@gmail.com>
2024-03-09 23:38:50 +0530

    current designation details component

---
+++ src/lib/components/ProfileSection/CurrentDesignationDetails.svelte
@@ -0,0 +1,5 @@
+<div class="flex flex-col items-end gap-2">
+	<img src="/icons/brand_icon_round.svg" alt="brand icon" width="35px" height="35px" />
+	<p class="text-base font-bold">Duggup</p>
+	<p class="text-xs font-normal text-[#7A9299]">Co-Founder and CEO</p>
+</div>
