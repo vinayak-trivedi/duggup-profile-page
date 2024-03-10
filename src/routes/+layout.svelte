d4a8cb8ef8bf60807d30018f7814f3848b2c8768
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2024-03-10 14:18:02 +0530

    Profile component (#2)
    * profile component completed till profile description
    
    * current designation details component
    
    * profile component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
 	import '../app.pcss';
-	import Navbar from '$lib/components/Navbar/Navbar.svelte';
 </script>
 
-<Navbar />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<main></main>
+<script>
+	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+	import ProfileSection from '$lib/components/ProfileSection/ProfileSection.svelte';
+</script>
+
+<Navbar />
+<main class="my-4 ml-auto mr-auto w-[95%] max-w-screen-lg gap-2.5">
+	<ProfileSection userImage="https://github.com/shadcn.png" username="Krishna Kiran" />
+</main>
